cd4ec675b65cc30137d485214e60bd158bdcb742
unknown <PBM@PBMP0202230110.ihc.id>
2026-09-25 16:51:00 +0700

    Fix Vercel serverless function routing and entrypoint

---
+++ api/cetak_laporan.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Vercel Serverless Entrypoint for Cetak Laporan
+ */
+require __DIR__ . '/../cetak_laporan.php';
---
+++ api/index.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Vercel Serverless Entrypoint for Kas Dosen UNPAM
+ */
+require __DIR__ . '/../index.php';
